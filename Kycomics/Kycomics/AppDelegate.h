7ef22eea372e26168a67837a567884c5be2cbb6b
HongYi <lihongyi1995@gmail.com>
2017-08-03 16:33:27 +0800

    【项目初始化】

---
+++ Kycomics/Kycomics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kycomics
+//
+//  Created by HongYi on 2017/8/3.
+//  Copyright © 2017年 kcvly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kycomics/Kycomics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kycomics
+//
+//  Created by HongYi on 2017/8/3.
+//  Copyright © 2017年 kcvly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
