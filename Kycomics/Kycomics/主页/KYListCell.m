89012e27d310cb042ea8706f075ac570afad347a
HongYi <lihongyi1995@gmail.com>
2017-08-18 14:03:57 +0800

    【cell显示优化】

--- Kycomics/Kycomics/主页/KYListCell.m
+++ Kycomics/Kycomics/主页/KYListCell.m
@@ -25,8 +25,19 @@
     self.labTitle.text = _comic.title_jpn.length? _comic.title_jpn: _comic.title;
     self.labFileCount.text = _comic.filecount;
     self.labRating.text = [NSString stringWithFormat:@"%.2f", _comic.rating];
-    self.thumbImageView.backgroundColor = [UIColor colorWithRed:arc4random_uniform(256) / 255.0 green:arc4random_uniform(256) / 255.0 blue:arc4random_uniform(256) / 255.0 alpha:0.3];
-//    [self.thumbImageView sd_setImageWithURL:[NSURL URLWithString:_comic.thumb]];
+    self.contentView.backgroundColor = [UIColor colorWithRed:arc4random_uniform(256) / 255.0 green:arc4random_uniform(256) / 255.0 blue:arc4random_uniform(256) / 255.0 alpha:0.3];
+    
+    self.thumbImageView.alpha = 0;
+    __weak typeof(self) weakSelf = self;
+    [self.thumbImageView sd_setImageWithURL:[NSURL URLWithString:_comic.thumb]
+                                  completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+                                      
+                                      [UIView animateWithDuration:0.4 animations:^{
+                                          __weak typeof(weakSelf) strongSelf = weakSelf;
+                                          strongSelf.thumbImageView.alpha = 1.0;
+                                          
+                                      }];
+    }];
 }
 
 @end
